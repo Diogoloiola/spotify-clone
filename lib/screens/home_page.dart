d991377cc7d75240c9a4bee643c48089ed1c6298
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-30 01:14:46 -0300

    adicionando a lista de podcasts ao projeto

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:spotify_clone/repositories/chart_repositorire.dart';
+import 'package:spotify_clone/repositories/resource.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -9,8 +11,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Map<String, List<Object>> data;
+
   @override
   Widget build(BuildContext context) {
+    getAllData();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Boa noite'),
@@ -48,23 +54,33 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.all(10),
         color: Colors.black,
         child: Column(
-          children: const [
+          children: [
             ListElements(
               title: 'Podcast',
               type: 1,
+              data: data['podcasts'],
             ),
           ],
         ),
       ),
     );
   }
+
+  void getAllData() {
+    setState(() {
+      Resource client = Resource('https://api.deezer.com/', {});
+      ChartRepositorie(client.dio).all().then((value) => {data = value});
+    });
+  }
 }
 
 class ListElements extends StatelessWidget {
   final String title;
   final int type;
+  final List<Object>? data;
 
-  const ListElements({Key? key, required this.title, this.type = 1})
+  const ListElements(
+      {Key? key, required this.title, this.type = 1, required this.data})
       : super(key: key);
 
   @override
@@ -89,21 +105,15 @@ class ListElements extends StatelessWidget {
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: [
-              Element(
-                width: 150,
-                height: 10,
-                type: type,
-              ),
-              Element(
-                width: 150,
-                height: 50,
-                type: type,
-              ),
-              Element(
-                width: 150,
-                height: 50,
-                type: type,
-              ),
+              ...data!.map<Widget>((info) {
+                var object = info as dynamic;
+                return Element(
+                    height: 100,
+                    width: 150,
+                    type: 1,
+                    pathImage: object.picture,
+                    id: object.id);
+              }).toList()
             ],
           ),
         )
@@ -116,12 +126,17 @@ class Element extends StatelessWidget {
   final double width;
   final double height;
   final int type;
-  const Element({
-    Key? key,
-    required this.width,
-    required this.height,
-    required this.type,
-  }) : super(key: key);
+  final String pathImage;
+  final int id;
+
+  const Element(
+      {Key? key,
+      required this.width,
+      required this.height,
+      required this.type,
+      required this.pathImage,
+      required this.id})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -134,7 +149,8 @@ class Element extends StatelessWidget {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(getRadius())),
           color: Colors.red,
-        ));
+        ),
+        child: Image.network(pathImage));
   }
 
   double getRadius() {
