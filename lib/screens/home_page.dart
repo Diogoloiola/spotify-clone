57d94154d949694d28d13062f2dc519a6e21795d
Diogoloiola <diogomouraloiola@gmail.com>
2022-02-02 22:40:11 -0300

    criando bottom navigation entre telas

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,6 +5,7 @@ import 'package:spotify_clone/helpers/chose_message.dart';
 import 'package:spotify_clone/helpers/list_widgets.dart';
 import 'package:spotify_clone/repositories/chart_repositorire.dart';
 import 'package:spotify_clone/repositories/resource.dart';
+import 'package:spotify_clone/routes/navigator.dart';
 import 'package:spotify_clone/theme/colors.dart';
 import 'package:spotify_clone/screens/search.dart';
 
@@ -16,143 +17,141 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _currentPage = 'Home';
+  List<String> pageKeys = ['Home', 'Search'];
+  final Map<String, GlobalKey<NavigatorState>> _navigatorKeys = {
+    'Home': GlobalKey<NavigatorState>(),
+    'Search': GlobalKey<NavigatorState>(),
+  };
   int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  void _selectTab(String tabItem, int index) {
+    if (tabItem == _currentPage) {
+      _navigatorKeys[tabItem]!.currentState!.popUntil((route) => route.isFirst);
+    } else {
+      setState(() {
+        _currentPage = pageKeys[index];
+        _selectedIndex = index;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Resource client = Resource('https://api.deezer.com/', {});
 
-    List<Widget> _widgetOptions = [
-      FutureBuilder(
-        future: ChartRepositorie(client.dio).all(),
-        builder: (BuildContext context,
-            AsyncSnapshot<Map<String, List<Object>>> snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data != null) {
-              EasyLoading.dismiss();
-              return ListView(
-                children: [
-                  ListElements(
-                    title: 'Podcast',
-                    type: 1,
-                    widgets: getWidgets(snapshot.data!['podcasts']),
-                  ),
-                  ListElements(
-                    title: 'Albums',
-                    type: 1,
-                    widgets: getWidgets(snapshot.data!['albums']),
-                  ),
-                  ListElements(
-                    title: 'Playlists',
-                    type: 1,
-                    widgets: getWidgets(snapshot.data!['playlists']),
-                  ),
-                  ListElements(
-                    title: 'Tracks',
-                    type: 1,
-                    widgets: getWidgets(snapshot.data!['tracks']),
-                  ),
-                ],
-              );
+    return WillPopScope(
+        onWillPop: () async {
+          final isFirstRouteInCurrentTab =
+              !await _navigatorKeys[_currentPage]!.currentState!.maybePop();
+          if (isFirstRouteInCurrentTab) {
+            if (_currentPage != "Page1") {
+              _selectTab("Page1", 1);
+
+              return false;
             }
           }
-          EasyLoading.show();
-          return const SizedBox();
+          // let system handle back button if we're on the first route
+          return isFirstRouteInCurrentTab;
         },
-      ),
-      const Search(),
-    ];
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(choseMessage(_selectedIndex)),
-        backgroundColor: ColorPalette.darkItermediare,
-        actions: const [
-          IconButton(
-            icon: FaIcon(
-              FontAwesomeIcons.bell,
-              color: Colors.white,
-            ),
-            tooltip: 'Notificações',
-            onPressed: null,
-          ),
-          IconButton(
-            icon: FaIcon(
-              FontAwesomeIcons.redoAlt,
-              color: Colors.white,
-            ),
-            tooltip: 'Show Snackbar',
-            onPressed: null,
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.settings,
-              color: Colors.white,
-            ),
-            tooltip: 'Show Snackbar',
-            onPressed: null,
-          ),
-        ],
-      ),
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          padding: const EdgeInsets.all(10),
-          color: ColorPalette.darkItermediare,
-          child: _widgetOptions[_selectedIndex]),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
-          width: double.maxFinite,
-          decoration: const BoxDecoration(
-            color: ColorPalette.darkItermediare,
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                      onTap: () => _onItemTapped(0),
-                      child: Column(
-                        children: const [
-                          Icon(
-                            Icons.home,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      )),
-                  GestureDetector(
-                    onTap: () => _onItemTapped(1),
-                    child: Column(
-                      children: const [
-                        Icon(
-                          Icons.search,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          'Pesquisar',
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              )
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(choseMessage(_selectedIndex)),
+            backgroundColor: ColorPalette.darkItermediare,
+            actions: const [
+              IconButton(
+                icon: FaIcon(
+                  FontAwesomeIcons.bell,
+                  color: Colors.white,
+                ),
+                tooltip: 'Notificações',
+                onPressed: null,
+              ),
+              IconButton(
+                icon: FaIcon(
+                  FontAwesomeIcons.redoAlt,
+                  color: Colors.white,
+                ),
+                tooltip: 'Show Snackbar',
+                onPressed: null,
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                ),
+                tooltip: 'Show Snackbar',
+                onPressed: null,
+              ),
             ],
           ),
-        ),
+          body: Stack(
+            children: [
+              _buildOffstageNavigator("Home"),
+              _buildOffstageNavigator("Search"),
+            ],
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Container(
+              height: 50.0,
+              width: double.maxFinite,
+              decoration: const BoxDecoration(
+                color: ColorPalette.darkItermediare,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            _selectTab(pageKeys[0], 0);
+                          },
+                          child: Column(
+                            children: const [
+                              Icon(
+                                Icons.home,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'Home',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          )),
+                      GestureDetector(
+                        onTap: () {
+                          _selectTab(pageKeys[1], 1);
+                        },
+                        child: Column(
+                          children: const [
+                            Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Pesquisar',
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Widget _buildOffstageNavigator(String tabItem) {
+    return Offstage(
+      offstage: _currentPage != tabItem,
+      child: TabNavigator(
+        navigatorKey: _navigatorKeys[tabItem]!,
+        tabItem: tabItem,
       ),
     );
   }
@@ -192,6 +191,90 @@ class ListElements extends StatelessWidget {
           ),
         )
       ]),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Resource client = Resource('https://api.deezer.com/', {});
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(choseMessage(1)),
+      //   backgroundColor: ColorPalette.darkItermediare,
+      //   actions: const [
+      //     IconButton(
+      //       icon: FaIcon(
+      //         FontAwesomeIcons.bell,
+      //         color: Colors.white,
+      //       ),
+      //       tooltip: 'Notificações',
+      //       onPressed: null,
+      //     ),
+      //     IconButton(
+      //       icon: FaIcon(
+      //         FontAwesomeIcons.redoAlt,
+      //         color: Colors.white,
+      //       ),
+      //       tooltip: 'Show Snackbar',
+      //       onPressed: null,
+      //     ),
+      //     IconButton(
+      //       icon: Icon(
+      //         Icons.settings,
+      //         color: Colors.white,
+      //       ),
+      //       tooltip: 'Show Snackbar',
+      //       onPressed: null,
+      //     ),
+      //   ],
+      // ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(10),
+          color: ColorPalette.darkItermediare,
+          child: FutureBuilder(
+            future: ChartRepositorie(client.dio).all(),
+            builder: (BuildContext context,
+                AsyncSnapshot<Map<String, List<Object>>> snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data != null) {
+                  EasyLoading.dismiss();
+                  return ListView(
+                    children: [
+                      ListElements(
+                        title: 'Podcast',
+                        type: 1,
+                        widgets: getWidgets(snapshot.data!['podcasts']),
+                      ),
+                      ListElements(
+                        title: 'Albums',
+                        type: 1,
+                        widgets: getWidgets(snapshot.data!['albums']),
+                      ),
+                      ListElements(
+                        title: 'Playlists',
+                        type: 1,
+                        widgets: getWidgets(snapshot.data!['playlists']),
+                      ),
+                      ListElements(
+                        title: 'Tracks',
+                        type: 1,
+                        widgets: getWidgets(snapshot.data!['tracks']),
+                      ),
+                    ],
+                  );
+                }
+              }
+              EasyLoading.show();
+              return const SizedBox();
+            },
+          )),
     );
   }
 }
